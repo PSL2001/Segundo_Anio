312cb2397e8e5a167e1b7caaaafd239f2975d969
PSL2001 <pablitosanchezlopez@gmail.com>
2021-09-23 10:50:57 +0200

    Cambiado plantilla

--- Entorno Servidor/Tema 1/plantilla.php
+++ Entorno Servidor/Tema 1/plantilla.php
@@ -7,7 +7,9 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <title>Document</title>
 </head>
-<body>
+<body style="background-color: cadetblue;">
+<div class="container mt-3">
+</div>
     
 </body>
 </html>
