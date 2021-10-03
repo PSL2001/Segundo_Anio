f877cf1a7d5e5741510650ba3a174c420085ca45
PSL2001 <65461723+PSL2001@users.noreply.github.com>
2021-10-03 16:54:26 +0200

    Diez

--- Entorno Servidor/Tema 1/Redacciones/Redaccion_1/diez.php
+++ Entorno Servidor/Tema 1/Redacciones/Redaccion_1/diez.php
@@ -2,7 +2,7 @@
 function CalcularEquacionCuadrado($a, $b, $c)
 {
     if ($a!=0) {
-        return (-$b + sqrt(($b^2) - (4*$a*$c)))/(2*$a);
+        return (-$b + sqrt(($b^2) - (4*$a*$c)))/(2*$a); //Esto siempre da NAN, no se como arreglarlo
     } else {
         return ($b^2)-(4*$a*$c);
     }
