57be0663e2796082be1278c49ce77d00200434a3
PSL2001 <pablitosanchezlopez@gmail.com>
2022-02-09 14:36:13 +0100

    Clase 9 de Febrero: Crud Jetstream; Terminado Editar

--- Entorno Servidor/Laravel/jetstream02_02_22/app/Http/Livewire/ShowPosts.php
+++ Entorno Servidor/Laravel/jetstream02_02_22/app/Http/Livewire/ShowPosts.php
@@ -3,19 +3,35 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-use Livewire\WithPagination;
+use Livewire\{WithPagination, WithFileUploads};
 use App\Models\Post;
 use Illuminate\Support\Facades\Storage;
 
 class ShowPosts extends Component
 {
     use WithPagination;
+    use WithFileUploads;
+
     public $search = "";
     public $campo = "id";
     public $orden = "desc";
+    public $isOpen = false;
+    public Post $post;
+    public $imagen;
 
     //Listeners (a veces vienen con el nombre de la misma funcion EJ: render)
     protected $listeners = ['renderizarPosts' => 'render'];
+    protected $rules = [
+        'post.titulo'=> "",
+        'post.contenido'=> ['required', 'string', 'min:8'],
+        'imagen'=>['nullable', 'image', 'max:1024']
+    ];
+
+    //Carga la clase
+    public function mount() {
+        //Hay que instanciar un post cuando se vaya a actualizar, ademas de tener las validaciones completas
+        $this->post = new Post;
+    }
 
     public function render()
     {
@@ -39,5 +55,29 @@ class ShowPosts extends Component
         $post->delete();
         //Emitimos la alerta (no hace falta renderizar la vista en este caso por que es la misma)
         $this->emit("alerta", "Post Borrado");
+    }
+
+    public function mostrarEdit(Post $post) {
+        $this->isOpen = true;
+        $this->post = $post;
+    }
+    //Metodo que realmente actualiza el registro
+    public function update() {
+        //Validamos
+        $this->validate([
+            'post.titulo'=>['required', 'string', 'unique:posts,titulo,'.$this->post->id]
+        ]);
+        //Comprobamos si se ha subido una imagen
+        //Si se da ese caso hay que borrar la antigua
+        if ($this->imagen) {
+            //Borramos la antigua imagen
+            Storage::delete($this->post->imagen);
+            //Guardamos la nueva
+            $imagen = $this->imagen->store('posts');
+            $this->post->imagen = $imagen;
+        }
+        $this->post->save();
+        $this->reset(['isOpen', 'imagen']);
+        $this->emit("alerta", "Post modificado con exito");
     }
 }
--- Entorno Servidor/Laravel/jetstream02_02_22/resources/views/livewire/show-posts.blade.php
+++ Entorno Servidor/Laravel/jetstream02_02_22/resources/views/livewire/show-posts.blade.php
@@ -25,7 +25,7 @@
                             Contenido <i class="fas fa-sort"></i>
                         </th>
                         <th scope="col" colspan="2">
-                            Role
+                            Acciones
                         </th>
                     </tr>
                 </thead>
@@ -51,10 +51,12 @@
                             <td class="px-6 py-4">
                                 {{ $item->contenido }}
                             </td>
-                            <td class="px-6 py-4 whitespace-nowrap">
-                                Editar
+                            <td class="px-6 py-4 w-16">
+                                <button wire:click="mostrarEdit({{$item}})" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
+                                    <i class="fas fa-edit"></i>
+                                </button>
                             </td>
-                            <td class="px-6 py-4">
+                            <td class="px-6 py-4 w-16">
                                 <button wire:click="borrar({{$item}})" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
                                     <i class="fas fa-trash"></i>
                                 </button>
@@ -71,4 +73,45 @@
     <div class="mt-2">
         {{ $posts->links() }}
     </div>
+    <!-- Ventana modal para editar un registro -->
+    <x-jet-dialog-modal wire:model="isOpen">
+        <x-slot name="title">
+            Editar Post
+        </x-slot>
+        <x-slot name="content">
+            <x-jet-label value="Titulo del Post" />
+            <x-jet-input type="text" placeholder="Titulo" class="mt-2 mb-4 w-full" wire:model.defer="post.titulo" />
+            <x-jet-input-error for="post.titulo" />
+            <x-jet-label value="Contenido del Post" />
+            <textarea wire:model.defer="post.contenido" class="w-full mt-2 mb-4  border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm" placeholder="Contenido del Post"></textarea>
+            <x-jet-input-error for="post.contenido" />
+            {{-- Comienzo input de Imagen --}}
+            <div class="grid mt-2 grid-cols-2 gap-4">
+                <div>
+                    <div class="flex justify-center">
+                        <div>
+                            <input class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0
+                            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" type="file" id="formFile" wire:model="imagen" accept="image/*">
+                            <x-jet-input-error for="imagen" />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    {{-- Pintamos la imagen aqui tanto la defecto como la que mande el usuario --}}
+                    @if ($imagen)
+                    <img src="{{$imagen->temporaryUrl()}}" class="object-cover object-center w-80">
+                    @else
+                    <img src="{{Storage::url($post->imagen)}}" class="object-cover object-center w-80">
+                    @endif
+                </div>
+            </div>
+            {{-- Fin input Imagen --}}
+        </x-slot>
+        <x-slot name="footer">
+            <button wire:click="update" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
+                <i class="fas fa-edit"></i> Editar
+            </button>
+        </x-slot>
+    </x-jet-dialog-modal>
+    <!-- Fin Ventana Modal -->
 </div>
