37946a21a17ca5508d9a572243f68eda5323b993
PSL2001 <65461723+PSL2001@users.noreply.github.com>
2022-03-03 23:45:12 +0100

    Tamos ready

--- Entorno Cliente/Tema_8/src/storage.js
+++ Entorno Cliente/Tema_8/src/storage.js
@@ -1,6 +1,9 @@
 function mostrarAlmacen(etiqueta) {
+    //Vaciamos la etiqueta
+    etiqueta.innerHTML = "";
     if (window.localStorage) {
         //LocalStorage esta activado
+        etiqueta.innerHTML += "<br>Claves de LocalStorage: <br>";
         for (let i = 0; i < localStorage.length; i++) {
             let clave = localStorage.key(i);
             etiqueta.innerHTML += "La clave '" + clave + "' contiene el valor '" + localStorage.getItem(clave) + "'<br/>";
@@ -9,4 +12,17 @@ function mostrarAlmacen(etiqueta) {
         //LocaStorage no esta activado
         throw new Error("LocalStorage no esta activado!");
     }
+
+    if (window.sessionStorage) {
+        //SessionStorage esta activado
+        etiqueta.innerHTML += "<hr>Claves de SessionStorage: <br>";
+        for (let i = 0; i < sessionStorage.length; i++) {
+            let clave = sessionStorage.key(i);
+            etiqueta.innerHTML += "La clave '" + clave + "' contiene el valor '" + sessionStorage.getItem(clave) + "'<br/>";
+        }
+    } else {
+        //SessionStorage no esta activado
+        throw new Error("SessionStorage no esta activado!");
+    }
+
 }
